fef8c6c4e968dd5f781909d6345a3a08d608db83
Nikita Shagan <nikita.ydisk@yandex.ru>
2022-12-22 18:08:07 +0500

    add picture popup

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const popupProfileEditor = document.querySelector('.popup_sort_profile');
 const popupPlacesEditor = document.querySelector('.popup_sort_place');
+const popupPicture = document.querySelector('.popup_sort_picture')
 const popupProfileEditorCloser = popupProfileEditor.querySelector('.popup__close');
 const popupPlacesEditorCloser = popupPlacesEditor.querySelector('.popup__close');
+const popupPictureCloser = popupPicture.querySelector('.popup__close');
 const editProfileButton = document.querySelector('.profile__edit-btn');
 const addPlaceButton = document.querySelector('.profile__add-btn');
 
@@ -14,6 +16,7 @@ const profileAbout = document.querySelector('.profile__description');
 const formPlacesElement = popupPlacesEditor.querySelector('.popup__form');
 const placesInputName = popupPlacesEditor.querySelector('.popup__input_kind_name');
 const placesInputLink = popupPlacesEditor.querySelector('.popup__input_kind_link');
+
 
 const placesList = document.querySelector('.places__list');
 
@@ -44,35 +47,30 @@ const initialPlaces = [
   }
 ];
 
-function openPopupProfileEditor() {
-  popupProfileEditor.classList.add('popup_opened');
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function popupProfileRender() {
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
 
-function openPopupPlacesEditor() {
-  popupPlacesEditor.classList.add('popup_opened');
-}
-
-function closePopupProfileEditor() {
-  popupProfileEditor.classList.remove('popup_opened');
-}
-
-function closePopupPlacesEditor() {
-  popupPlacesEditor.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 function handleFormSubmitProfile(evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  closePopupProfileEditor()
+  closePopup(popupProfileEditor);
 }
 
 function handleFormSubmitPlaces(evt) {
   evt.preventDefault();
   addPlace(placesInputName.value, placesInputLink.value);
-  closePopupPlacesEditor();
+  closePopup(popupPlacesEditor);
   placesInputName.value = '';
   placesInputLink.value = '';
 }
@@ -80,15 +78,25 @@ function handleFormSubmitPlaces(evt) {
 function addPlace(name, link) {
   const placeTemplate = document.querySelector('#place').content;
   const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
-  placeElement.querySelector('.place__name').textContent = name;
-  placeElement.querySelector('.place__image').src = link;
-  placesList.prepend(placeElement);
-  console.log(placeElement);
 
   const deleteButton = placeElement.querySelector('.place__delete');
   const likeButton = placeElement.querySelector('.place__like');
+  const picture = placeElement.querySelector('.place__image');
+  const pictureZoomed = document.querySelector('.zoomed-picture__image');
+  const pictureCaption = document.querySelector('.zoomed-picture__caption');
+
+  placeElement.querySelector('.place__name').textContent = name;
+  picture.src = link;
+  picture.alt = name;
+  placesList.prepend(placeElement);
+
   deleteButton.addEventListener('click', () => placeElement.remove());
   likeButton.addEventListener('click', () => likeButton.classList.toggle('place__like_active'));
+  picture.addEventListener('click', () => {
+    openPopup(popupPicture);
+    pictureZoomed.src = link;
+    pictureCaption.textContent = name;
+  });
 }
 
 initialPlaces.forEach(item => addPlace(item.name, item.link));
@@ -96,8 +104,13 @@ initialPlaces.forEach(item => addPlace(item.name, item.link));
 formProfileElement.addEventListener('submit', handleFormSubmitProfile);
 formPlacesElement.addEventListener('submit', handleFormSubmitPlaces);
 
-editProfileButton.addEventListener('click', openPopupProfileEditor);
-addPlaceButton.addEventListener('click', openPopupPlacesEditor);
+editProfileButton.addEventListener('click', () => {
+  openPopup(popupProfileEditor);
+  popupProfileRender();
+});
 
-popupProfileEditorCloser.addEventListener('click', closePopupProfileEditor);
-popupPlacesEditorCloser.addEventListener('click', closePopupPlacesEditor);
+addPlaceButton.addEventListener('click', () => openPopup(popupPlacesEditor));
+
+popupProfileEditorCloser.addEventListener('click', () => closePopup(popupProfileEditor));
+popupPlacesEditorCloser.addEventListener('click', () => closePopup(popupPlacesEditor));
+popupPictureCloser.addEventListener('click', () => closePopup(popupPicture));
