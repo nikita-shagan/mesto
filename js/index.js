6568b94a12f7a5d68505cf14d3d9ced06d91dbea
Nikita Shagan <nikita.ydisk@yandex.ru>
2022-12-22 16:09:54 +0500

    feat: add functionality to delete cards

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const popupPlacesEditor = document.querySelector('.popup_sort_place');
 const popupProfileEditorCloser = popupProfileEditor.querySelector('.popup__close');
 const popupPlacesEditorCloser = popupPlacesEditor.querySelector('.popup__close');
 const editProfileButton = document.querySelector('.profile__edit-btn');
-const addCardButton = document.querySelector('.profile__add-btn');
+const addPlaceButton = document.querySelector('.profile__add-btn');
 
 const formProfileElement = popupProfileEditor.querySelector('.popup__form');
 const inputName = popupProfileEditor.querySelector('.popup__input_kind_name');
@@ -17,7 +17,7 @@ const placesInputLink = popupPlacesEditor.querySelector('.popup__input_kind_link
 
 const placesList = document.querySelector('.places__list');
 
-const initialCards = [
+const initialPlaces = [
   {
     name: 'Алтай',
     link: '../images/altai.jpg'
@@ -71,30 +71,33 @@ function handleFormSubmitProfile(evt) {
 
 function handleFormSubmitPlaces(evt) {
   evt.preventDefault();
-  addCard(placesInputName.value, placesInputLink.value);
+  addPlace(placesInputName.value, placesInputLink.value);
   closePopupPlacesEditor();
   placesInputName.value = '';
   placesInputLink.value = '';
 }
 
-function addCard(name, link) {
-  const cardTemplate = document.querySelector('#place').content;
-  const cardElement = cardTemplate.querySelector('.place').cloneNode(true);
-  cardElement.querySelector('.place__name').textContent = name;
-  cardElement.querySelector('.place__image').src = link;
-  placesList.prepend(cardElement);
+function addPlace(name, link) {
+  const placeTemplate = document.querySelector('#place').content;
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+  placeElement.querySelector('.place__name').textContent = name;
+  placeElement.querySelector('.place__image').src = link;
+  placesList.prepend(placeElement);
+  console.log(placeElement);
+
+  const deleteButton = placeElement.querySelector('.place__delete');
+  const likeButton = placeElement.querySelector('.place__like');
+  deleteButton.addEventListener('click', () => placeElement.remove());
+  likeButton.addEventListener('click', () => likeButton.classList.toggle('place__like_active'));
 }
 
-initialCards.forEach(item => addCard(item.name, item.link));
-
-const likeButtons = document.querySelectorAll('.place__like');
-Array.from(likeButtons).forEach(item => item.addEventListener('click', () => item.classList.toggle('place__like_active')));
+initialPlaces.forEach(item => addPlace(item.name, item.link));
 
 formProfileElement.addEventListener('submit', handleFormSubmitProfile);
 formPlacesElement.addEventListener('submit', handleFormSubmitPlaces);
 
 editProfileButton.addEventListener('click', openPopupProfileEditor);
-addCardButton.addEventListener('click', openPopupPlacesEditor);
+addPlaceButton.addEventListener('click', openPopupPlacesEditor);
 
 popupProfileEditorCloser.addEventListener('click', closePopupProfileEditor);
 popupPlacesEditorCloser.addEventListener('click', closePopupPlacesEditor);
