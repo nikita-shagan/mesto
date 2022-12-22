cede8a5d5e2d7dbf06e6c7c471a27826047c873b
Nikita Shagan <nikita.ydisk@yandex.ru>
2022-12-22 11:27:10 +0500

    feat: add initial cards

--- js/index.js
+++ js/index.js
@@ -1,13 +1,41 @@
-let popup = document.querySelector('.popup');
-let popupCloser = popup.querySelector('.popup__close');
-let popupOpener = document.querySelector(".profile__edit-btn");
+const popup = document.querySelector('.popup');
+const popupCloser = popup.querySelector('.popup__close');
+const popupOpener = document.querySelector(".profile__edit-btn");
 
-let formElement = popup.querySelector('.popup__form');
-let inputName = popup.querySelector('.popup__input_kind_name');
-let inputAbout = popup.querySelector('.popup__input_kind_about');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__description');
+const formElement = popup.querySelector('.popup__form');
+const inputName = popup.querySelector('.popup__input_kind_name');
+const inputAbout = popup.querySelector('.popup__input_kind_about');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__description');
 
+const placesList = document.querySelector('.places__list');
+
+const initialCards = [
+  {
+    name: 'Алтай',
+    link: '../images/altai.jpg'
+  },
+  {
+    name: 'Судак',
+    link: '../images/sudak.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: '../images/baikal.jpg'
+  },
+  {
+    name: 'Ельбрус',
+    link: '../images/elbrus.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: '../images/kamchatka.jpg'
+  },
+  {
+    name: 'Шерегеш',
+    link: '../images/sheregesh.jpg'
+  }
+];
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -28,6 +56,15 @@ function handleFormSubmit(evt) {
   closePopup()
 }
 
+function addCard(name, link) {
+  const cardTemplate = document.querySelector('#place').content;
+  const cardElement = cardTemplate.querySelector('.place').cloneNode(true);
+  cardElement.querySelector('.place__name').textContent = name;
+  cardElement.querySelector('.place__image').src = link;
+  placesList.append(cardElement);
+}
+
+initialCards.forEach(item => addCard(item.name, item.link));
 
 formElement.addEventListener('submit', handleFormSubmit);
 popupOpener.addEventListener('click', openPopup)
