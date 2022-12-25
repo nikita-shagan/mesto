c20372ed83efe4910f4e36e644a147355efbc84d
Nikita Shagan <nikita.ydisk@yandex.ru>
2022-12-25 13:08:12 +0500

    refactor: split the code into more functions

--- js/index.js
+++ js/index.js
@@ -21,18 +21,69 @@ const placeInputLink = popupPlacesEditor.querySelector('.popup__input_kind_link'
 
 const placesList = document.querySelector('.places__list');
 
+
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
+
 
 function renderPopupProfile() {
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
 
+
+function openPopupProfile() {
+  openPopup(popupProfileEditor);
+  renderPopupProfile();
+}
+
+
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
+
+
+function putLike(evt) {
+  evt.target.classList.toggle('place__like_active');
+}
+
+
+function openZoomedPicture(name, link) {
+  const pictureZoomed = document.querySelector('.zoomed-picture__image');
+  const pictureCaption = document.querySelector('.zoomed-picture__caption');
+  openPopup(popupPicture);
+  pictureZoomed.src = link;
+  pictureZoomed.alt = name;
+  pictureCaption.textContent = name;
+}
+
+
+function createPlace(name, link) {
+  const placeTemplate = document.querySelector('#place').content;
+  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+
+  const placeButtonDelete = placeElement.querySelector('.place__delete');
+  const placeButtonLike = placeElement.querySelector('.place__like');
+  const placeName = placeElement.querySelector('.place__name');
+  const placePicture = placeElement.querySelector('.place__image');
+
+  placeName.textContent = name;
+  placePicture.src = link;
+  placePicture.alt = name;
+
+  placeButtonDelete.addEventListener('click', () => placeElement.remove());
+  placeButtonLike.addEventListener('click', putLike);
+  placePicture.addEventListener('click', () => openZoomedPicture(name, link));
+
+  return placeElement;
+}
+
+
+function addPlace(place, container) {
+  container.prepend(place);
+}
+
 
 function handleFormSubmitProfile(evt) {
   evt.preventDefault();
@@ -40,6 +91,7 @@ function handleFormSubmitProfile(evt) {
   profileAbout.textContent = inputAbout.value;
   closePopup(popupProfileEditor);
 }
+
 
 function handleFormSubmitPlaces(evt) {
   evt.preventDefault();
@@ -50,50 +102,27 @@ function handleFormSubmitPlaces(evt) {
   placeInputLink.value = '';
 }
 
-function createPlace(name, link) {
-  const placeTemplate = document.querySelector('#place').content;
-  const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
 
-  const buttonDelete = placeElement.querySelector('.place__delete');
-  const likeButton = placeElement.querySelector('.place__like');
-  const picture = placeElement.querySelector('.place__image');
-  const pictureZoomed = document.querySelector('.zoomed-picture__image');
-  const pictureCaption = document.querySelector('.zoomed-picture__caption');
-
-  placeElement.querySelector('.place__name').textContent = name;
-  picture.src = link;
-  picture.alt = name;
-
-  buttonDelete.addEventListener('click', () => placeElement.remove());
-  likeButton.addEventListener('click', () => likeButton.classList.toggle('place__like_active'));
-  picture.addEventListener('click', () => {
-    openPopup(popupPicture);
-    pictureZoomed.src = link;
-    pictureZoomed.alt = name;
-    pictureCaption.textContent = name;
+function renderInitialPlaces() {
+  initialPlaces.forEach(item => {
+    const placeNew = createPlace(item.name, item.link)
+    addPlace(placeNew, placesList);
   });
-  return placeElement;
 }
 
-function addPlace(place, container) {
-  container.prepend(place);
+
+function addEventListeners () {
+  formProfileElement.addEventListener('submit', handleFormSubmitProfile);
+  formPlacesElement.addEventListener('submit', handleFormSubmitPlaces);
+
+  popupProfileOpenButton.addEventListener('click', openPopupProfile);
+  popupPlacesOpenButton.addEventListener('click', () => openPopup(popupPlacesEditor));
+
+  popupProfileEditorCloser.addEventListener('click', () => closePopup(popupProfileEditor));
+  popupPlacesEditorCloser.addEventListener('click', () => closePopup(popupPlacesEditor));
+  popupPictureCloser.addEventListener('click', () => closePopup(popupPicture));
 }
 
-initialPlaces.forEach(item => {
-  const placeNew = createPlace(item.name, item.link)
-  addPlace(placeNew, placesList);
-});
 
-formProfileElement.addEventListener('submit', handleFormSubmitProfile);
-formPlacesElement.addEventListener('submit', handleFormSubmitPlaces);
-
-popupProfileOpenButton.addEventListener('click', () => {
-  openPopup(popupProfileEditor);
-  renderPopupProfile();
-});
-
-popupPlacesOpenButton.addEventListener('click', () => openPopup(popupPlacesEditor));
-
-popupProfileEditorCloser.addEventListener('click', () => closePopup(popupProfileEditor));
-popupPlacesEditorCloser.addEventListener('click', () => closePopup(popupPlacesEditor));
-popupPictureCloser.addEventListener('click', () => closePopup(popupPicture));
+renderInitialPlaces()
+addEventListeners()
